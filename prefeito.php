a9e55de575323e249fcc350cb7efe662f9053b2f
nando454545 <72951302+nando454545@users.noreply.github.com>
2020-11-19 22:27:44 -0300

    Add files via upload

---
+++ prefeito.php
@@ -0,0 +1,720 @@
+<html>
+<head>
+
+  <meta name="theme-color" content="green">
+
+
+
+
+<style>
+
+    *{
+      margin:2px;
+      padding:0px;
+    }
+     .corpo{
+
+
+      background-color:red;
+
+    width:1000%;
+    height:150%;
+  display:inline;
+
+
+    }
+    .v2{
+      width:220px;
+      height:250px;
+
+
+
+    }
+    .v1{
+
+
+      width:220px;
+      height:250px;
+
+    }
+    .v3{
+      width:200px;
+      height:200px;
+
+
+    }
+    .v4{
+
+      width:200px;
+      height:200px;
+
+    }
+    .v5{
+      width:200px;
+      height:200px;
+
+    }
+    .v6{
+      width:200px;
+      height:200px;
+
+
+    }
+    .v7{
+      width:200px;
+      height:200px;
+
+
+    }
+
+    .v8{
+     width:200px;
+      height:200px;
+
+
+
+    }
+    .v9{
+      width:200px;
+      height:200px;
+
+    }
+    .v10{
+      width:200px;
+      height:200px;
+
+    }
+    .a{
+      padding:10px;
+     background-color:#ccc;
+      margin-left:6%;
+      width:800px;
+      height:400px;
+      text-align:center;
+      margin-top:50px;
+    }
+
+
+
+
+
+
+    button{
+      background-color:#fff;
+     margin-left:8%;
+      border:none;
+
+    }
+    .cabeca{width:100%;
+
+      height:200px;
+      padding:10px;
+      background-color:#BDE6BA;
+      margin-bottom:20px;
+
+    }
+    .cabeca p{
+      margin-top:10px;
+
+      text-align:center;
+      height:50px;
+      width:700px;
+      margin-left:10%;
+      font-size:200%;
+      border-radius:8%;
+
+       background-color:green;
+      color:#ccc;
+
+
+    }
+    body{
+
+      background-color:#BDE6BA;
+    }
+    .cabeca .pesquisa {
+      margin-top:10px;
+      margin-left:38%;
+      border-radius:15px;
+       height:50px;
+      width:300px;
+      font-size:230%;
+      padding:10px;
+      background-color:green;
+      color:#fff;
+      text-align:center;
+    }
+
+    .cabeca .prefeito{
+      width:170px;
+      height:30px;
+      background-color:green;
+      color:#fff;
+      text-align:center;
+      border-radius:8px;
+      font-size:120%;
+
+
+
+
+    }.containe{
+
+      width:100%;
+      height:70px;
+     background-color:#fff;
+
+    }
+    .roda{
+
+      height:400px;
+      width:100%;
+
+margin-top:400px;
+      color:red;
+      background-color:#fff;
+    }
+
+      .cabesa{
+        width:98%;
+        height:80px;
+        background-color:green;
+        text-align:center;
+        color:#fff;
+        font-size:35px;
+        border-radius:5px;
+        position:;
+        display:block;
+
+      }
+  .vot{
+      width:93%;
+      height:90px;
+      font-size:20px;
+      margin-top:20px;
+      background-color:#ccc;
+      text-align:center;
+border:5px double;
+    margin-left:2%;
+
+    }.vot button{
+      background-color:green;
+      color:#fff;
+      border-radius:5px;
+      font-size:40px;
+      border:8px solid #fffafd;
+       margin-top:8px;
+    }
+  a{
+
+
+    color:#2a730c;
+    font-size:30pt;
+   text-decoration:none;
+  }
+*{
+
+	font-family: arial;
+}
+
+.container-urna{
+	background-color: #CEC8BC;
+	padding: 10px;
+	width: 100%;
+	height: 380px;
+	margin-left: auto;
+	margin-right: auto;
+}
+
+.fim-voto{
+	text-align: center;
+	margin-top: 16%;
+	margin-bottom: 10%;
+	font-size: 45px;
+	display: none;
+}
+
+.container-painel{
+	background-color: #E6E7E6;
+	float: left;
+	width: 63%;
+	border: solid 2px #AAA79E;
+}
+
+.container-info{
+	float: left;
+	padding: 10px;
+}
+
+.container-info label{
+	font-size: 1rem;
+}
+
+.container-info span{
+	font-size: 1rem;
+}
+
+.input{
+	width: 40px;
+	height:30px;
+	background-color:#FFF;
+	border: 2px solid #cec8bc;
+	text-align: center;
+  font-size: 2rem;
+  font-weight: 700;
+}
+
+.container-foto{
+	float: right;
+}
+
+.container-foto img{
+	width: 150px;
+    height:200px;
+}
+
+.container-instrucao{
+	width: 100%;
+	float: left;
+	margin-top: 20px;
+}
+
+.intrucao{
+	padding-left: 10px;
+	padding-top: 5px;
+	padding-bottom: 5px;
+	border: 1px solid #AAA79E;
+}
+
+.container-teclado{
+	background-color: #453C3D;
+	float: right;
+	width: 35%;
+}
+
+.container-numbers{
+	width: 100%;
+}
+
+.teclas{
+	width: 90%;
+	margin: 10px auto;
+}
+
+.tecla-number{
+	background-color: #000;
+    color: #FFF;
+    width: 28%;
+    height: 50px;
+    float: left;
+    margin-right: 5px;
+    margin-bottom: 16px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 35px;
+}
+
+.tecla-hidden{
+	background-color: #453C3D;
+}
+
+.tecla-cursor{
+	cursor: pointer;
+}
+
+.container-actions{
+	width: 100%;
+	margin-top: 10px;
+}
+
+.tecla-action{
+	display: flex;
+   	align-items: center;
+   	justify-content: center;
+	font-size: 10px;
+   	font-weight: 700;
+  	margin-left: 2.5%;
+	width: 30%;
+	height: 45px;
+	margin-bottom: 10px;
+	cursor: pointer;
+}
+
+.action-branco{
+	background-color: #FFF;
+	float: left;
+}
+
+.action-corrige{
+	background-color: #FF7700;
+	float: left;
+}
+
+.action-confirma{
+	background-color: #33CC99;
+	float: left;
+}
+
+.container-resultado{
+	margin-left: 10px;
+    margin-top: 20px;
+    display: none;
+}
+
+.container-resultado h3{
+	text-align: center;
+}
+
+.restart{
+	display: flex;
+   	align-items: center;
+   	justify-content: center;
+	font-size: 20px;
+   	font-weight: 700;
+  	margin-left: auto;
+  	margin-right: auto;
+	width: 40%;
+	height: 45px;
+	margin-bottom: 10px;
+	cursor: pointer;
+  background-color: #21A5F3;}
+.restart{
+	display: flex;
+   	align-items: center;
+   	justify-content: center;
+	font-size: 20px;
+   	font-weight: 700;
+  	margin-left: auto;
+  	margin-right: auto;
+	width: 40%;
+	height: 45px;
+	margin-bottom: 10px;
+	cursor: pointer;
+  background-color: #21A5F3;}
+  .urna{
+    width:100%;
+    height:100%;
+    margin-top:70px;
+
+
+
+  }
+.modal{
+             width:100%;
+           height:100%;
+           position:absolute;
+           background-color: rgba(0,0,0,0.75);
+           transition:600ms;
+           z-index:2;
+           top:-100%;
+
+         }.mo{
+           width:60%;
+           height:80px;
+           background-color:#fff;
+           position:absolute;
+           margin:20px 60px;
+           font-size:25px;
+           text-align:center;
+           color:green;
+           border:4px  ;
+
+         }.mo1{
+           position:absolute;
+           background-color:#ccc;
+           display:block;
+           margin-top:110px;
+           width:92%;
+           height:50px;
+           margin-left:2%;
+           font-size:23px;
+           border:2px solid green;
+           text-align:center;
+         }.mo1 button{
+           background-color:green;
+           margin:0px 0px 0px 0px;
+           border-radius:5px;
+           border:5px solid red;
+
+
+          border:5px solid;
+           color:#fff;
+           margin-top:10px;
+           font-size:18px;}
+  .resulte{
+
+   font-size:50px;
+    background-color:green;
+    color:#fff;
+  }
+
+  </style>
+
+
+
+  </head>
+<body>
+  <div class ="cabesa">
+      pesquisa para prefeito/  Cuitegi
+      </div>
+
+  <div class= "corpo">
+
+     <div class="a">
+   <button id="lelo"onclick="abri()"value="1">
+      <img src="vere/chico.jpg"class="v1"><h1>Chico<br>
+      Mala <p style='color:#58FA82'>23</p> </h1>
+     </button>
+       <!--outro button -->
+    <button id="lelo2"onclick="abri2()"value="2">
+       <img src="vere/geraldo.jpg"class="v2"><h1>Geraldo <br>serafin<p style='color:#58FA82'>45</p> </h1>
+      </button>
+       <!--outro button-->
+
+</div>
+
+
+    <div class ="vot">
+      resultado da pesquisa
+    <a href="home.php" ><button onclick="game()"id="jogador">resultado</button>
+     </a>
+    </div>
+
+
+
+   <div class="urna" id="oi">
+  	<div class="container-urna">
+		<div class="container-painel">
+			<div class="container-resultado" id="container-resultado">
+				<h3>Total de Votos</h3>
+				<label>Deadpool: </label><span id="deapool"></span>
+				<br>
+				<label>Cyborg: </label><span id="cyborg"></span>
+				<br>
+				<label>Robin: </label><span id="robin"></span>
+				<br>
+				<label>Ravena: </label><span id="ravena"></span>
+				<br>
+				<label>Estrelar: </label><span id="estrelar"></span>
+				<br>
+				<label>Mutano: </label><span id="mutano"></span>
+				<br>
+				<label>Brancos: </label><span id="brancos"></span>
+				<br>
+				<label>Nulos: </label><span id="nulos"></span>
+				<br>
+				<button class="restart" onclick="inicio()">
+					Nova Votação
+				</button>
+			</div>
+			<div class="fim-voto" id="fim-voto">
+				<h1>FIM</h1>
+			</div>
+			<div class="container-hidden" id="container-hidden">
+				<div class="container-info">
+					<h5>SEU VOTO PARA PREFEITO</h5>
+					<br>
+					<label>NUMER: </label>
+					<input class="input" type="number"  id="digito1"disabled >
+
+				<input class="input" type="number" value="" id="digito2" disabled>
+
+				<input class="input" type="number" value="" id="digito3" disabled>
+
+
+
+					<input class="input" type="number" value="" id="digito4" disabled>
+
+
+					<input class="input" type="number" value="" id="digito5" disabled>
+
+
+
+
+					<br>
+					<br>
+					<label>Nome: </label><span id="nome"></span>
+					<br>
+					<label>Partido: </label><span id="partido"></span>
+				</div>
+				<div class="container-foto">
+					<img src="i/branco.jpg" alt="deadpool" id="imgPresidente">
+				</div>
+			</div>
+			<div class="container-instrucao">
+				<div class="intrucao">
+					<p>Aperte a tecla:</p>
+					<p>VERDE para CONFIRMAR </p>
+					<p>LARANJA para REINICIAR </p>
+				</div>
+			</div>
+		</div>
+		<div class="container-teclado">
+			<div class="container-numbers">
+				<div class="teclas">
+					<div class="tecla-number tecla-cursor" id="tecla-1" onclick="digitado(this.id)">1</div>
+					<div class="tecla-number tecla-cursor" id="tecla-2" onclick="digitado(this.id)">2</div>
+
+          <div class=" tecla-number tecla-cursor" id="tecla-3" onclick="digitado(this.id)">3</div>
+
+					<div class="tecla-number tecla-cursor" id="tecla-4" onclick="digitado(this.id)">4</div>
+					<div class="tecla-number tecla-cursor" id="tecla-5" onclick="digitado(this.id)">5</div>
+					<div class="tecla-number tecla-cursor" id="tecla-6" onclick="digitado(this.id)">6</div>
+					<div class="tecla-number tecla-cursor" id="tecla-7" onclick="digitado(this.id)">7</div>
+					<div class="tecla-number tecla-cursor" id="tecla-8" onclick="digitado(this.id)">8</div>
+					<div class="tecla-number tecla-cursor" id="tecla-9" onclick="digitado(this.id)">9</div>
+					<div class="tecla-number tecla-hidden"></div>
+					<div class="tecla-number tecla-cursor" id="tecla-0" onclick="digitado(this.id)">0</div>
+					<div class="tecla-number tecla-hidden"></div>
+				</div>
+			</div>
+			<div class="container-actions">
+				<div class="tecla-action action-branco" onclick="votoBranco()">
+
+					BRANCO
+				</div>
+
+				<div class="tecla-action action-corrige" onclick="corrigir()">
+					CORRIGE
+				</div>
+				<div class="tecla-action action-confirma" onclick="conf()"id="jogador">
+					CONFIRMA
+				</div>
+			<div>
+		</div>
+  </div>
+          </div>
+
+        <a href="home.php">  <button class="resulte">resltado</button></a>
+
+
+
+
+<div class="modal" id="no">
+
+
+  <div class="mo">
+    seu voto foi computado
+           ✓
+
+  </div>
+  <div class="mo1">
+
+    resutado da pesquisa <a href="home.php"><button id="jogador">resutado</button></a>
+  </div>
+
+
+
+  <script>
+
+
+  function conf(){
+
+    document.getElementById('no').style.top="0px";
+
+
+  }
+
+
+
+var vel;
+var dx;
+var dy;
+var px;
+var jog;
+var anima;
+
+function inicia(){
+
+  vel=4;
+  dx=2;
+  dy=0;
+  px=0;
+  jog=document.getElementById("jogador");
+
+
+game();
+
+}
+   function game(){
+
+     px=px+(dx*vel);
+     jog.style.border="8px solid red"
+
+     jog.style.borderRadius=px+"%";
+    if(px > 80){
+      dx=-1;
+
+
+   }else if(px < 0 ){
+       dx=1   }
+    anima=requestAnimationFrame(game);
+
+
+   }
+
+    function para(){
+
+    cancelAnimationFrame(anima);
+    }
+    window.addEventListener("load",inicia);
+
+
+
+function abri(){
+         if(document.getElementById('lelo').innerHTMLValue="1" == 1){
+
+
+ document.getElementById('imgPresidente').src="vere/chico.jpg" ;
+ document.getElementById('nome').innerHTML="Chico mala";
+ document.getElementById('partido').innerHTML="Cidadania"
+document.getElementById('digito1').placeholder="2";
+document.getElementById('digito2').placeholder="3";
+        document.getElementById('digito3').placeholder="";
+ document.getElementById('digito4').placeholder=""
+      document.getElementById('digito5').placeholder="";
+
+           document.getElementById('oi').style.display="block";
+         }
+
+
+     }
+
+
+function abri2(){
+         if(document.getElementById('lelo2').innerHTMLValue="2" == 2){
+
+
+ document.getElementById('imgPresidente').src="vere/geraldo.jpg" ;
+ document.getElementById('nome').innerHTML="geraldo serafin";
+ document.getElementById('partido').innerHTML="PSDB"
+document.getElementById('digito1').placeholder="4";
+document.getElementById('digito2').placeholder="5";
+        document.getElementById('digito3').placeholder="";
+ document.getElementById('digito4').placeholder=""
+      document.getElementById('digito5').placeholder="";
+
+           document.getElementById('oi').style.display="block";
+         }
+
+
+     }
+
+
+
+
+
+  </script>
+
+
+
+  </body>
+
+
+
+
+
+
+
+
+</html>
